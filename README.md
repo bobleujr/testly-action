63f6a5b0773548bcd7238372a20662b8cbfff5ec
Paulo Tabarro <paulogtabarro@gmail.com>
2022-04-09 12:05:29 -0400

    add testly js action

---
+++ README.md
@@ -0,0 +1,5 @@
+# To generate build
+```
+npm install @vercel/ncc
+ncc build index.js -o dist
+```
--- index.js
+++ index.js
@@ -1,124 +1,32 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs');
+const axios = require('axios');
 
-const test = process.env.GITHUB_RUN_ID;
+const main = async() => {
+    const runId = process.env.GITHUB_RUN_ID;
+    const repository = process.env.GITHUB_REPOSITORY;
+    const apiKey = core.getInput('api_key', { required: true });
+    const file = core.getInput('file', { required: true });
+    const data = fs.readFileSync(file, 'utf8')
 
-console.log(test)
-
-const main2 = async () => {
-  try {
-    /**
-     * We need to fetch all the inputs that were provided to our action
-     * and store them in variables for us to use.
-     **/
-    //const owner = core.getInput('owner', { required: true });
-    //const repo = core.getInput('repo', { required: true });
-    //const pr_number = core.getInput('pr_number', { required: true });
-    //const token = core.getInput('token', { required: true });
-    
-    /**
-     * Now we need to create an instance of Octokit which will use to call
-     * GitHub's REST API endpoints.
-     * We will pass the token as an argument to the constructor. This token
-     * will be used to authenticate our requests.
-     * You can find all the information about how to use Octokit here:
-     * https://octokit.github.io/rest.js/v18
-     **/
-    const octokit = new github.getOctokit(token);
-
-    /**
-     * We need to fetch the list of files that were changes in the Pull Request
-     * and store them in a variable.
-     * We use octokit.paginate() to automatically loop over all the pages of the
-     * results.
-     * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
-     */
-    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
-      owner,
-      repo,
-      pull_number: pr_number,
+    const body = JSON.stringify({
+        "api_key": apiKey,
+        "repository": repository.replace("/", "-"),
+        "run": `${runId}`,
+        "junit": data
     });
 
-
-    /**
-     * Contains the sum of all the additions, deletions, and changes
-     * in all the files in the Pull Request.
-     **/
-    let diffData = {
-      additions: 0,
-      deletions: 0,
-      changes: 0
-    };
-
-    // Reference for how to use Array.reduce():
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-    diffData = changedFiles.reduce((acc, file) => {
-      acc.additions += file.additions;
-      acc.deletions += file.deletions;
-      acc.changes += file.changes;
-      return acc;
-    }, diffData);
-
-    /**
-     * Loop over all the files changed in the PR and add labels according 
-     * to files types.
-     **/
-    for (const file of changedFiles) {
-      /**
-       * Add labels according to file types.
-       */
-      const fileExtension = file.filename.split('.').pop();
-      switch(fileExtension) {
-        case 'md':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['markdown'],
-          });
-        case 'js':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['javascript'],
-          });
-        case 'yml':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['yaml'],
-          });
-        case 'yaml':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['yaml'],
-          });
-      }
-    }
-
-    /**
-     * Create a comment on the PR with the information we compiled from the
-     * list of changed files.
-     */
-    await octokit.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number: pr_number,
-      body: `
-        Pull Request #${pr_number} has been updated with: \n
-        - ${diffData.changes} changes \n
-        - ${diffData.additions} additions \n
-        - ${diffData.deletions} deletions \n
-      `
-    });
-
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    axios.post('https://us-east1-testly-github.cloudfunctions.net/push_junit', body, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log('Error: ', err.message);
+        });
 }
 
 // Call the main function to run the action
